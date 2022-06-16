ec7fec0352b8cee745efc0224c4f082c8c7661fd
Vladislav <vlad1988_1@mail.ru>
2022-06-16 17:19:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Data;
+using System.Runtime.Serialization.Formatters.Binary;
+namespace HomeWork_8_Task_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string file = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Students.dat";
+
+        if (File.Exists(file))
+        {
+            BinaryFormatter formatter = new();
+
+            using (var fs = new FileStream(file, FileMode.Open))
+            {
+                //var student = formatter.Deserialize(fs) as Student;  // Ошибка десериализации
+
+                //var student = formatter.Deserialize(fs) as Student[];  // Ошибка десереализации
+
+                //var student = formatter.Deserialize(fs) as DataTable;  // Ошибка десереализации
+
+                //var student = formatter.Deserialize(fs) as DataSet;  // Ошибка десереализации
+
+                //var student = formatter.Deserialize(fs) as DataTableCollection;  // Маловероятно. Ошибка десереализации
+
+                //var student = formatter.Deserialize(fs) as BinaryFormatter;  // Крайне маловероятно. Ошибка десереализации
+            }
+
+        }
+        Console.ReadKey();
+    }
+}
